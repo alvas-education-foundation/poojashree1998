b8d86ad6d08d65ef3430653f4b37f8720f79c783
poojashree1998 <32675280+poojashree1998@users.noreply.github.com>
2020-05-25 20:01:30 +0530

    Create coding1.py

---
+++ 23-05-2020/coding1.py
@@ -0,0 +1,15 @@
+  
+#remove ith element
+def remove(string, i):  
+  
+    for j in range(len(string)): 
+        if j == i: 
+            string = string.replace(string[i], "", 1) 
+    return string 
+      
+
+if __name__ == '__main__': 
+      
+    string = "geeksFORgeeks"
+    i = 5 
+    print(remove(string, i)) 
