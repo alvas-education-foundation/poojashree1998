a3198be019198391b2fb866202c83163c1196bcd
poojashree1998 <32675280+poojashree1998@users.noreply.github.com>
2020-06-03 17:06:13 +0530

    Create coding1.c

---
+++ 03-06-2020/coding1.c
@@ -0,0 +1,16 @@
+//program to print half pyramid using stars
+#include <stdio.h>
+int main()
+{
+    int i, j,n;
+    scanf(“%d”,&n);
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j <= i; j++)
+        {
+            printf(“*”);
+        }
+        printf(“\n”);
+    }
+    return 0;
+}
