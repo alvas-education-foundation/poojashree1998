7b50333cbe9ff9200aa8abb4c35dea16b9515e66
poojashree1998 <32675280+poojashree1998@users.noreply.github.com>
2020-06-19 18:38:55 +0530

    Create coding1.c

---
+++ 19-06-2020/coding1.c
@@ -0,0 +1,29 @@
+#include  <stdio.h>
+int main()
+{
+    
+    //Initializing variables.
+    char str[100], str_no_spc[100];
+    int i=0, j=0 ;
+    
+    //Accepting inputs.
+    printf("Enter the string:\n");
+    gets(str);
+
+    //Iterating each character of string. 
+    while(str[i] != '\0')    
+    {
+        if(str[i] != ' ')  
+        {
+            
+            str_no_spc[j++] = str[i];
+        }
+        
+        i++;
+    }
+    str_no_spc[j] = '\0';
+    //Printing result.
+    printf("The string after removing all the spaces is:\n%s", str_no_spc);
+    
+    return 0;
+}
