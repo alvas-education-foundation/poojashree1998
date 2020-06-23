f23f3e4f1e36f0e2324cd0c2495a79fec806801a
poojashree1998 <32675280+poojashree1998@users.noreply.github.com>
2020-06-23 19:33:18 +0530

    Create coding1.py

---
+++ 23-06-2020/coding1.py
@@ -0,0 +1,8 @@
+Char =  input() 
+#Check if the Char belong to set of Vowels
+if (Char == 'a' or Char == 'e' or Char == 'i' or Char == 'o' or Char == 'u'): 
+    #if true 
+    print("Character is Vowel") 
+else: 
+    #if false
+    print("Character is Consonant")
