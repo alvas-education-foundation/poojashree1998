117f0e749ec2487ca86bec28243e2a38489879a2
poojashree1998 <32675280+poojashree1998@users.noreply.github.com>
2020-07-02 12:07:46 +0530

    Create coding.c

---
+++ 02-07-2020/coding.c
@@ -0,0 +1,18 @@
+counting the number of vowels by using the null character.
+
+#include<stdio.h>  
+void main ()  
+{  
+    char s[11] = "javatpoint";  
+    int i = 0;   
+    int count = 0;  
+    while(s[i] != NULL)  
+    {  
+        if(s[i]=='a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'u' || s[i] == 'o')  
+        {  
+            count ++;  
+        }  
+        i++;  
+    }  
+    printf("The number of vowels %d",count);  
+}  
