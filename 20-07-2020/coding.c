9822173939125e04b370488955ccec12d6e9dd6f
poojashree1998 <32675280+poojashree1998@users.noreply.github.com>
2020-07-21 16:25:45 +0530

    Update coding.c

--- 20-07-2020/coding.c
+++ 20-07-2020/coding.c
@@ -1,38 +1,31 @@
-#include <stdio.h>
-#include <math.h>
+/*C program to print following Pyramid:
+        0
+       01
+      010
+     0101
+    01010
+*/
 
-int fac(int x)
-{
-	int i,fac=1;
-	for(i=1;i<=x;i++)
-		fac=fac*i;
-	return fac;
-}
+#include<stdio.h>
 
 int main()
 {
-	float x,Q,sum=0;
-	int i,j,limit;
-
-	printf("Enter the value of x of sinx series: ");
-	scanf("%f",&x);
-
-	printf("Enter the limit upto which you want to expand the series: ");
-	scanf("%d",&limit);
-
-	Q=x;
-	x = x*(3.1415/180);
-
-	for(i=1,j=1;i<=limit;i++,j=j+2)
+	int i,j,k;
+	
+	for(i=0 ; i<=4 ; i++)
 	{
-		if(i%2!=0)
-		{
-			sum=sum+pow(x,j)/fac(j);
-		}
-		else
-			sum=sum-pow(x,j)/fac(j);
-	}
+		for(j=4 ; j>i ; j--)
+			printf(" ");
 
-	printf("Sin(%0.1f): %f",Q,sum);
-	return 0;
+		for(k=0 ; k<=i; k++)
+		{
+			if(k%2==0)
+				printf("0");
+			else
+				printf("1");
+		}
+		printf("\n");
+	}
+    
+    return 0;
 }
